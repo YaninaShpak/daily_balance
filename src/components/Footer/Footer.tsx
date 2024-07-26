a223bcd2380a51324f72f0ade1bac100cdc7b4c5
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-07-26 11:31:56 +0300

    feat: create component Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.root}>
+      <div className="container">2024 | Ianina Shpak</div>
+    </footer>
+  );
+};
+
+export default Footer;
