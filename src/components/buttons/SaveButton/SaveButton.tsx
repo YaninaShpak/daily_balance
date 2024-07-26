91c5645075cbf366b9fec8c5e6e246bf0fdd851f
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-07-26 11:31:35 +0300

    feat: create component SaveButton

---
+++ src/components/buttons/SaveButton/SaveButton.tsx
@@ -0,0 +1,10 @@
+
+import styles from './SaveButton.module.css';
+
+const SaveButton = () => {
+  return (
+    <button className={styles.root} type='button'>Сохранить</button>
+  );
+};
+
+export default SaveButton;
