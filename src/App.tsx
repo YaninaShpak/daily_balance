72dd640592a32dd378a9d37de6745abc16fd07dd
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:44:52 +0300

    feat: add expense, component Savings and Result

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import './App.css'
+import { useEffect } from 'react'
+import { useAppSelector, useAppDispatch } from './hooks'
+import { setIncome, setExpense, setTotalIncome, setTotalExpenses } from './redux/slices/initialMoneySlice'
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import SaveButton from './components/buttons/SaveButton/SaveButton'
 import EditableList from './components/EditableList/EditableList'
-import { useAppSelector } from './hooks'
-import { setIncome } from './redux/slices/initialMoneySlice'
+import Savings from './components/Savings/Savings'
+import Result from './components/Result/Result'
+
+import './App.css'
 
 function App() {
-  const {income} = useAppSelector((state) => state.initialMoney);
+  const dispatch = useAppDispatch();
+  const { income, expense, totalIncome, totalExpenses } = useAppSelector((state) => state.initialMoney);
+
+  useEffect(() => {
+    dispatch(setTotalIncome());
+    dispatch(setTotalExpenses())
+  }, [income, expense])
 
   return (
-    <div className='app'>
+    <div className="app">
       <div className="wrapper">
         <Header />
-        <main className='mainWrapper'>
-          <div className="container">
+        <main>
+          <div className="container mainWrapper">
             <div className="editableWrapper">
-              <EditableList title="Доход" items={income} setFunction={setIncome}/>
+              <EditableList title="Доходы" items={income} setFunction={setIncome} total={totalIncome}/>
+              <EditableList title="Расходы" items={expense} setFunction={setExpense} total={totalExpenses}/>
             </div>
+            <div className="rowWrapper">
+              <Savings/>
+              <Result/>
+            </div>
+            <SaveButton />
           </div>
-          <div className="container">
-            <SaveButton/>
-          </div>
-          
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   )
