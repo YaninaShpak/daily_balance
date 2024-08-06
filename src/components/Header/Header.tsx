6d96192889e7b996e9ac9f935a66d70424343562
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:48:40 +0300

    feat: add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,9 @@ const Header = () => {
   return (
     <header className={styles.root}>
       <div className={`container ${styles.wrapper}`}>
-        <h1>Мой бюджет</h1>
-        <p>
-          Если указать свою зарплату и обязательные траты, калькулятор
-          рассчитает ежедневный бюджет.
+        <h1 className="mainTitle">Мой бюджет</h1>
+        <p className={styles.text}>
+          Если указать свою зарплату и обязательные траты, калькулятор рассчитает ежедневный бюджет
         </p>
       </div>
     </header>
