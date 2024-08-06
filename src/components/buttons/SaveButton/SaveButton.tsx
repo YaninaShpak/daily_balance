3498a4f4a7384493a3e7edee69cbb7944bd21026
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:50:03 +0300

    feat: add localStorage

--- src/components/buttons/SaveButton/SaveButton.tsx
+++ src/components/buttons/SaveButton/SaveButton.tsx
@@ -1,9 +1,17 @@
-
+import { useAppSelector } from '../../../hooks';
 import styles from './SaveButton.module.css';
 
 const SaveButton = () => {
+  const {income, expense, savings} = useAppSelector((state) => state.initialMoney);
+
+  function saveToLocalStorage() {
+    localStorage.setItem("income", JSON.stringify(income));
+    localStorage.setItem("expense", JSON.stringify(expense));
+    localStorage.setItem("savings", JSON.stringify(savings));
+  }
+
   return (
-    <button className={styles.root} type='button'>Сохранить</button>
+    <button className={styles.root} type="button" onClick={saveToLocalStorage}>Сохранить</button>
   );
 };
 
