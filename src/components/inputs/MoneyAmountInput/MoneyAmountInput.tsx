a42e5b0caafe818a293ecb211bec3edc463c5697
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:52:08 +0300

    feat: add functionality in component MoneyAmountInput

--- src/components/inputs/MoneyAmountInput/MoneyAmountInput.tsx
+++ src/components/inputs/MoneyAmountInput/MoneyAmountInput.tsx
@@ -1,20 +1,77 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, KeyboardEvent, FC, useEffect, useState } from 'react';
+import ErrorInputMessage from '../../ErrorInputMessage/ErrorInputMessage';
+
 import styles from './MoneyAmountInput.module.css'
 
 interface MoneyAmountInputProps {
   value: number,
-  handleFunction: (e: ChangeEvent<HTMLInputElement>) => void
+  handleFunction: (value: string) => void
 }
 
 const MoneyAmountInput: FC<MoneyAmountInputProps> = ({value, handleFunction}) => {
+  const [localValue, setLocalValue] = useState(`${value.toLocaleString('ru-RU')} ₽/мес`);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const newValue = isFocused ? value.toLocaleString('ru-RU') : `${value.toLocaleString('ru-RU')} ₽/мес`;
+    setLocalValue(newValue)
+  }, [value, isFocused]);
+
+  const formatNumber = (num: string): string => {
+    // Удаление всех нецифровых символов
+    const cleanNum = num.replace(/\D/g, '');
+    // Преобразование в число и форматирование
+    return Number(cleanNum).toLocaleString('ru-RU');
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.search(/[^0-9]/ig) !== -1) {
+      setIsError(true)
+    }
+    const formatted = formatNumber(e.target.value)
+    setLocalValue(formatted);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setLocalValue(value.toLocaleString('ru-RU'))
+  };
+
+  const handleBlurOrEnter = () => {
+    setIsFocused(false);
+    setIsError(false);
+    const numericValue = localValue.replace(/\D/g, '');
+    if (numericValue === '') {
+      handleFunction('0');
+      setLocalValue('0 ₽/мес');
+    } else {
+      handleFunction(numericValue);
+      setLocalValue(`${formatNumber(numericValue)} ₽/мес`);
+    }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleBlurOrEnter();
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <input
-      className={styles.root}
-      type="text"
-      placeholder="Сумма"
-      value={value}
-      onChange={handleFunction}
-    />
+    <div className={styles.rootWrapper}>
+      <input
+        className={styles.root}
+        type="text"
+        placeholder="Сумма"
+        value={localValue}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlurOrEnter}
+        onKeyDown={handleKeyDown}
+      />
+      {isError && <ErrorInputMessage message="Введите число"/>}
+    </div>
   );
 };
 
