8e358eb050eda7c13de70931dfcacf923ce91f6a
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:53:22 +0300

    feat: add totalResult

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -2,14 +2,15 @@ import { useAppSelector } from "../../hooks";
 import styles from "./Result.module.css";
 
 const Result = () => {
-  const {savings, totalResult, dailyBudget} = useAppSelector((state) => state.initialMoney);
+  const { savings, totalResult, dailyBudget } = useAppSelector((state) => state.initialMoney);
 
   return (
     <section className={styles.root}>
-      <h2>Мой бюджет на день: <span>{Math.round(dailyBudget)} ₽</span></h2>
-      <div>Общий бюджет на месяц: {totalResult} ₽</div>
-      <div>Накоплю за месяц: {savings} ₽</div>
-      <div>Накоплю за год: {savings * 12} ₽</div>
+      <h2 className="title">Мой бюджет на день: <span>{Math.round(dailyBudget).toLocaleString('ru-RU')} ₽</span></h2>
+      <p>Бюджет до вычета сбережений: {totalResult.toLocaleString('ru-RU')} ₽</p>
+      <p>Бюджет на месяц: {(totalResult - savings).toLocaleString('ru-RU')} ₽</p>
+      <p>Накоплю за месяц: {savings.toLocaleString('ru-RU')} ₽</p>
+      <p>Накоплю за год: {(savings * 12).toLocaleString('ru-RU')} ₽</p>
     </section>
   );
 };
