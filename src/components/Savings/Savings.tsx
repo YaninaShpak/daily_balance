5b6cb571b945756218b22267b216855b037ab42a
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-06 12:53:50 +0300

    feat: add component ErrorInputMessage

--- src/components/Savings/Savings.tsx
+++ src/components/Savings/Savings.tsx
@@ -1,35 +1,39 @@
+import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { setSavings } from "../../redux/slices/initialMoneySlice";
+
+import ErrorInputMessage from "../ErrorInputMessage/ErrorInputMessage";
 import MoneyAmountInput from "../inputs/MoneyAmountInput/MoneyAmountInput";
 import RangeInput from "../inputs/RangeInput/RangeInput";
+
 import styles from "./Savings.module.css";
 
 const Savings = () => {
   const dispatch = useAppDispatch();
-  const {savings, totalResult} = useAppSelector((state) => state.initialMoney);
+  const { savings, totalResult } = useAppSelector((state) => state.initialMoney);
+  const [isError, setIsError] = useState(false);
 
   const onHandleChange = (value: string) => {
     const numberValue = Number(value);
     if (isNaN(numberValue)) {
-      alert('Ошибка')
-      
+      alert('Ошибка');
     } else if (numberValue > totalResult){
-      alert('Не должно превышать сумму дохода')
+      setIsError(true);
     } else {
+      setIsError(false);
       dispatch(setSavings(numberValue));
     }
-
-    
   };
 
   return (
     <section className={styles.root}>
-      <h2>Сбережения</h2>
+      <h2 className="title">Сбережения</h2>
       <RangeInput/>
       <MoneyAmountInput
         value={savings}
-        handleFunction={(e) => onHandleChange(e.target.value)}
+        handleFunction={(e) => onHandleChange(e)}
       />
+      {isError && <ErrorInputMessage message="Не должно превышать бюджет до вычета сбережений"/>}
     </section>
   );
 };
