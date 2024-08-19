71ff8946dac9093942769027e1d0ab7d9b222f72
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-19 18:20:17 +0300

    feat: add link in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.root}>
-      <div className="container">2024 | <a className="link" href="#">Ianina Shpak</a></div>
+      <div className="container">2024 | <a className="link" href="https://ianina-shpak-cv.vercel.app/projects">Ianina Shpak</a></div>
     </footer>
   );
 };
